227c61c02ebe7177387d7398820a25a0063c5350
André Batista <hi@andrebatista.dev>
2021-11-22 13:09:08 +0000

    fix: missing exports

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
 export { createContextFromSchema } from './createContextFromSchema';
 
-export type { State, Actions, ContextSchema, ContextType } from './types';
+export type { State, Actions, ContextSchema, CreateContextFromSchema } from './types';
